f7d11ca3f67e0449ba3854c410ce1999c5986bf7
ngreene14 <54511282+ngreene14@users.noreply.github.com>
2019-09-25 12:56:34 -0400

    Class notes on php file

--- app/public/api/patients.php
+++ app/public/api/patients.php
@@ -1,11 +1,18 @@
 <?php
 
 // Step 1: Get a datase connection from our help class
+//Dbconnection class tells php to do the autoload function built in composer
+//-> is for instance methods; :: is for static classes; always return the same connection
+//db object has new instance of PDO object to interact with database
+//
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
+//prepares SQL statement to be executed and returns statement object
 $stmt = $db->prepare('SELECT * FROM Patient');
+//actually execute the PDO prepare statement object; runs query, gets results and ready to give to me
 $stmt->execute();
+//procure all of the results; always give an array
 $patients = $stmt->fetchAll();
 
 // patientGuid VARCHAR(64) PRIMARY KEY,
@@ -15,8 +22,12 @@ $patients = $stmt->fetchAll();
 // sexAtBirth CHAR(1) DEFAULT ''
 
 // Step 3: Convert to JSON
+//give serialized json representation of the variable (turn back into php -->json_decode)
+//$json is a string variable
 $json = json_encode($patients, JSON_PRETTY_PRINT);
 
 // Step 4: Output
+//what the webpage is expecting to get --> browser is expecting json
 header('Content-Type: application/json');
+//print the returned json variable in the webpage
 echo $json;
